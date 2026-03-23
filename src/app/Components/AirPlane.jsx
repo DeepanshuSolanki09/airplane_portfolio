d90fe785a6fa0f00a098290879f84efd5194c1eb
DeepanshuSolanki09 <solankideepanshu2006@gmail.com>
2026-03-24 00:04:50 +0530

    update

--- src/app/Components/AirPlane.jsx
+++ src/app/Components/AirPlane.jsx
@@ -1,25 +1,36 @@
 "use client";
-import { useGLTF } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
-import React, { useRef } from 'react'
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
 
 const AirPlane = () => {
+  const model = useGLTF("/model.glb");
+  const helix = useRef();
 
-    const model = useGLTF('/model.glb');
-    const helix = useRef();
-
-    useFrame((state,delta) => {
-        helix.current.rotation.x += delta * 30;
-    })
+  useFrame((state, delta) => {
+    helix.current.rotation.x += delta * 30;
+  });
   return (
     <>
-      <group dispose={null} rotation={[0,-Math.PI/2,0]} scale={[0.2,0.2,0.2]}>
-        <mesh geometry={model.nodes.PUSHILIN_Plane_Circle000.geometry} material={model.nodes.PUSHILIN_Plane_Circle000.material} />
+      <group
+        dispose={null}
+        rotation={[0, -Math.PI / 2, 0]}
+        scale={[0.2, 0.2, 0.2]}
+      >
+        <mesh geometry={model.nodes.PUSHILIN_Plane_Circle000.geometry}>
+          <meshStandardMaterial color="#C5C6D0"/>
+        </mesh>
 
-        <mesh ref={helix} geometry={model.nodes.PUSHILIN_Plane_Helix.geometry} material={model.nodes.PUSHILIN_Plane_Helix.material} position={[1.09, 0.23, 0]}/>
+        <mesh
+          ref={helix}
+          geometry={model.nodes.PUSHILIN_Plane_Helix.geometry}
+          position={[1.09, 0.23, 0]}
+        >
+          <meshStandardMaterial color={"white"}/>
+        </mesh>
       </group>
     </>
-  )
-}
+  );
+};
 
-export default AirPlane
+export default AirPlane;
