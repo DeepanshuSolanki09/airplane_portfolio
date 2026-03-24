f38c86d91f05c47a13056aea90843b52cda3114e
DeepanshuSolanki09 <solankideepanshu2006@gmail.com>
2026-03-24 14:45:09 +0530

    update

--- src/app/Components/Scene.jsx
+++ src/app/Components/Scene.jsx
@@ -2,20 +2,26 @@
 import Image from "next/image";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, ScrollControls } from "@react-three/drei";
-import React from 'react'
-import  Experience from "./Experience";
+import React, { Suspense } from "react";
+import Experience from "./Experience";
 
 const Scene = () => {
   return (
     <div className="h-screen w-screen">
-      <Canvas className="h-screen w-screen" camera={{ position: [0, 0, 2] }}>
-        <ScrollControls pages={70} damping={0.1} wrapperClass="scroll-container">
-          {/* <OrbitControls /> */}
-          <Experience/>
-        </ScrollControls>
-      </Canvas>
+      {/* <Suspense fallback={null}> */}
+        <Canvas className="h-screen w-screen" camera={{ position: [0, 0, 2] }}>
+          <ScrollControls
+            pages={70}
+            damping={0.01}
+            wrapperClass="scroll-container"
+          >
+            {/* <OrbitControls /> */}
+            <Experience />
+          </ScrollControls>
+        </Canvas>
+      {/* </Suspense> */}
     </div>
-  )
-}
+  );
+};
 
-export default Scene
+export default Scene;
