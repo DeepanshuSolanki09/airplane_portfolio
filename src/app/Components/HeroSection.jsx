6aeb47501e807db535b8ec6ad87e299f1da34715
DeepanshuSolanki09 <solankideepanshu2006@gmail.com>
2026-03-24 22:15:45 +0530

    update

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -39,14 +39,14 @@ const HeroSection = () => {
   };
 
   return (
-    <div className="h-screen w-screen font-custom p-5 sticky bg-gradient-to-b from-blue-400 to-white flex items-center justify-center text-white gap-5 flex-col target absolute top-0 left-0 z-20">
+    <div className="min-h-screen w-screen font-custom p-5 sticky bg-gradient-to-b from-blue-400 to-white flex items-center justify-center text-white gap-5 flex-col target absolute top-0 left-0 z-20">
       <div>
-        <h1 className="lg:text-7xl text-4xl font-extrabold main">DEEPANSHU SOLANKI</h1>
+        <h1 className="lg:text-7xl text-6xl font-extrabold main text-center">DEEPANSHU SOLANKI</h1>
         <p  className="lg:text-2xl text-xl text-center main2">I'm a creative web developer</p>
       </div>
-      <button onClick={animater} className="bg-blue-950 font-bold py-2 px-3 hover:bg-white hover:text-black transition-all duration-300 lg:text-2xl w-1/8 rounded-full cursor-pointer btn1">Explore</button>
+      <button onClick={animater} className="bg-blue-950 font-bold py-2 px-3 hover:bg-white hover:text-black transition-all duration-300 lg:text-2xl lg:w-1/8 rounded-full cursor-pointer btn1">Explore</button>
 
-       <button onClick={() => window.open("/contact", "_blank")}  className="bg-blue-950 font-bold py-2 px-3 hover:bg-white hover:text-black transition-all duration-300 lg:text-md w-1/10 rounded-full cursor-pointer absolute bottom-5 right-5 btn2">Contact</button>
+       <button onClick={() => window.open("/contact", "_blank")}  className="bg-blue-950 font-bold py-2 px-3 hover:bg-white hover:text-black transition-all duration-300 lg:text-md lg:w-1/10 rounded-full cursor-pointer absolute bottom-5 right-5 btn2">Contact</button>
 
        <img src={'/Photos/1000008249.png'} className="plane absolute top-0 bottom-0 h-[100px] w-[100px] object-cover"/>
     </div>
